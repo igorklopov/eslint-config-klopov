cceae3726bd4d767aed6770fedda6b3335025bba
IgorKlopov <igor@klopov.com>
2019-12-08 00:36:23 +0300

    print if rules are possibly outdated

--- bin.js
+++ bin.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
+const major1 = require('eslint/package.json').version.match(/(\d+)/)[1];
+const major2 = require('./package.json').devDependencies.eslint.match(/[~^]?(\d+)/)[1];
+
+if (major1 !== major2) {
+  console.log('[eslint-config-klopov] eslint major version mismatch. the rules are outdated');
+}
+
 require('eslint/bin/eslint.js');
